f9f8c06888cf8500b701bdbcb6a43d775c1e7b18
Howard King <rjxx007@sina.com>
2014-03-07 09:28:18 +0800

    add the hello_world.module

---
+++ hello_world_m/Hello_world.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void)
+{
+	printk(KERN_ALERT "Hello, world!--HowardKing\n");
+	printk(KERN_INFO "The process is \"%s\" (pid %i)\n", \
+		current->comm, current->pid);
+
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_INFO "The process is \"%s\" (pid %i)\n", \
+		current->comm, current->pid);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("HowardKing <tianlangroot@gmail.com>");
+MODULE_VERSION("1.0");
+MODULE_DESCRIPTION("This is my hello world module");
